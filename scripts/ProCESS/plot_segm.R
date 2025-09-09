1168e266d0726e0b6c9c8373767bd56e7ad02b44
valerianilucrezia <valerianilucrezia11@gmail.com>
2025-09-09 10:00:06 +0200

    evaluation minor

---
+++ scripts/ProCESS/plot_segm.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+base = '/orfeo/scratch/area/lvaleriani/utils_locate/simulations_rRACES/out/clonal'
+sims = paste0('sim_', 1:30)
+
+tol = 10
+w_size = 5
+thr = 1e-15
+mode = 'max'
+suffix = paste(tol, mode, w_size, thr, sep = '_') 
+
+df <- tibble()
+for(sim in sims){
+  path = file.path(base, sim)
+  combinations = list.dirs(path)[grepl('cov', list.dirs(path))]
+  for (comb in combinations){
+    metrics = read.csv(file.path(comb, paste0('bp_metrics_', suffix, '.csv'))) %>% 
+      tibble() %>% 
+      tidyr::separate(run_id, sep = '_', into = c('sim_name', 'sim', 'cov_name', 'cov', 'pur_name', 'pur', 'mode', 'w_size', 'thr')) %>% 
+      select(-X, -sim_name, -cov_name, -pur_name)
+    df <- bind_rows(df, metrics)
+  }
+}
+
+all <- df %>% 
+  pivot_longer(cols = c(precision, recall, f1)) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = name, y = value, fill = cov)) +
+  xlab('metric') +
+  facet_grid(.~pur) + 
+  ggtitle('Tolerance = 10bp') + 
+  theme_minimal() 
+print(df)
---
+++ scripts/ProCESS/run_inf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=GENOA
+#SBATCH --job-name=seg_ProCESS_locate
+#SBATCH --nodes=1
+#SBATCH --array=1-30
+####SBARCH --tasks-per-node=1
+#SBARCH --cpus-per-task=2
+#SBATCH --mem 50g
+#SBATCH --time=48:00:00
+#SBATCH --output=seq_%a.out
+####SBATCH --output=sim.out
+
+sim=${SLURM_ARRAY_TASK_ID}
+echo $sim
+
+export PATH="/orfeo/scratch/area/lvaleriani/myconda/bin:$PATH"
+source activate locate
+
+PYTHONPATH="/orfeo/scratch/area/lvaleriani/locate/"
+export PYTHONPATH="/orfeo/scratch/area/lvaleriani/locate/"
+
+export PATH="/orfeo/scratch/area/lvaleriani/myconda/envs/locate/bin:/orfeo/scratch/area/lvaleriani/myconda/condabin:/orfeo/scratch/area/lvaleriani/myconda/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/u/area/lvaleriani/.local/bin:/u/area/lvaleriani/bin"
+
+#python segment_ProCESS.py -b /orfeo/scratch/area/lvaleriani/utils_locate/simulations_rRACES/out/clonal -s sim_${sim}
+python evaluate_seg_ProCESS.py -b /orfeo/scratch/area/lvaleriani/utils_locate/simulations_rRACES/out/clonal -s sim_${sim} 
