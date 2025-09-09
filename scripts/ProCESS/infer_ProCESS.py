d58fa1b7548de90127020f5115dd837ed883cba4
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2025-09-09 14:40:21 +0200

    add run and evaluation of cn

--- scripts/ProCESS/infer_ProCESS.py
+++ scripts/ProCESS/infer_ProCESS.py
@@ -7,68 +7,77 @@ from pyro.infer import TraceMeanField_ELBO, TraceEnum_ELBO, TraceGraph_ELBO, Tra
 import pandas as pd
 import torch
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from typing import Dict, Tuple, Optional
+import re
 
 import argparse
 import os
 
 import locate.locate as l
 from locate.models import Clonal
+from locate.utils import estimate_ploidy
+from locate.evaluation.metric import score_cn_predictions
 
-print(torch.cuda.is_available())
 
-def read_data(in_file, data_type = 'ill'):
-    #tmp_csv = f'{in_dir}/tmp_{data_type}_{ch}.csv'    
-
+def read_data(in_file):
     data = pd.read_csv(in_file, sep = ',', on_bad_lines='skip') 
     data['pos'] = range(1, len(data) + 1)
+    data['baf'] = data['median_baf'].apply(lambda x: 1 - x if x > 0.5 else x)
+    data = data[data['baf']>0]
+    data_input = {'baf':torch.tensor(np.array(data.baf).reshape(-1, 1)),
+                'dr':torch.tensor(np.array(data.median_dr).reshape(-1, 1)),
+                'dp_snp':torch.tensor(np.array(data.mean_dp).reshape(-1, 1)),
+                'orginal_baf':torch.tensor(np.array(data.median_baf).reshape(-1, 1)),
+                'vaf':torch.tensor(np.array(data.vaf).reshape(-1, 1)),
+                'dp_snv':torch.tensor(np.array(data.DP).reshape(-1, 1))}
     
-    if data_type == 'ill':
-        data['original_BAF'] = data['BAF_ILL']
-        data['BAF_ILL'] = data['BAF_ILL'].apply(lambda x: 1.01 - x if x > 0.5 else x)
-        data['BAF_ILL'] = data['BAF_ILL'].apply(lambda x: 0.01 if x == 0 else x)
-        
-        data = data[data['BAF_ILL']>0]
-        data_input = {'baf':torch.tensor(np.array(data.BAF_ILL).reshape(-1, 1)),
-                      'dr':torch.tensor(np.array(data.DR_ILL).reshape(-1, 1)),
-                      'dp_snp':torch.tensor(np.array(data.DP_ILL).reshape(-1, 1)),
-                      'orginal_baf':torch.tensor(np.array(data.original_BAF).reshape(-1, 1))}
-    
-    elif data_type == 'np':
-        data['original_BAF'] = data['BAF_NP']
-        data['BAF_NP'] = data['BAF_NP'].apply(lambda x: 1.01 - x if x > 0.5 else x)
-        data['BAF_NP'] = data['BAF_NP'].apply(lambda x: 0.01 if x == 0 else x)
-        
-        data = data[data['BAF_NP']>0]
-        data_input = {'baf':torch.tensor(np.array(data.BAF_NP).reshape(-1, 1)),
-                      'dr':torch.tensor(np.array(data.DR_NP).reshape(-1, 1)),
-                      'dp_snp':torch.tensor(np.array(data.DP_NP).reshape(-1, 1)),
-                      'orginal_baf':torch.tensor(np.array(data.original_BAF).reshape(-1, 1))}
-    
-    return data_input
+    return data, data_input
 
-def run_hmm(data_input, gpu = False, nsteps = 400):
+def run_hmm(data_input, 
+            gpu = False, 
+            nsteps = 100, 
+            fix_ploidy = True, 
+            vaf = False, 
+            ploidy = None,
+            bps = None):
+    
     locate = l.LOCATE(CUDA = gpu)
 
     locate.set_model(Clonal)
     locate.set_optimizer(ClippedAdam)
     locate.set_loss(TraceEnum_ELBO)
-    locate.initialize_model({"baf": data_input["baf"],
-                            "dr": data_input["dr"], 
-                            "dp_snp": data_input["dp_snp"], 
-                            "vaf": None, 
-                            "dp": None
-                            })
+    
+    if vaf:
+        locate.initialize_model({"baf":data_input["baf"],
+                                "dr":data_input["dr"], 
+                                "dp_snp":data_input["dp_snp"], 
+                                "vaf": data_input["vaf"], 
+                                "dp": data_input["dp"]})
+    else:
+        locate.initialize_model({"baf":data_input["baf"],
+                                "dr":data_input["dr"], 
+                                "dp_snp":data_input["dp_snp"], 
+                                "vaf": None, 
+                                "dp": None})
 
+    ploidy = [ploidy if ploidy is not None else 0]
     locate.set_model_params({"jumping_prob" : 1e-6,
                             "fix_purity": False,
-                            "prior_purity": 0.99,
-                            "prior_ploidy": 2,
+                            "fix_ploidy" : fix_ploidy, 
+                            "prior_purity": 0,
+                            "prior_ploidy": ploidy,
                             "scaling_factors": [1,1,1],
-                            "prior_bp": False,
-                            'init_probs': torch.tensor([0.1, 0.1, 0.1, 0.1, 0.1])
-                            })
+                            'hidden_dim': 4,
+                            "prior_bp": bps,
+                            "lambda_cn": 1.0,          # ΔCN penalty slope used to shape the prior
+                            "alpha_conc": 50.0,        # global Dirichlet concentration (higher = stronger prior)
+                            "alpha_self_boost": 3.0,   # extra mass on the diagonal
+                            "alpha_dip_boost": 0.5,    # extra mass on diploid column (if present)
+                            "bp_strength": 3.0})
     
-    ll = locate.run(steps = nsteps, param_optimizer = {"lr" : 0.05})
+    ll = locate.run(steps = nsteps, param_optimizer = {"lr" : 0.05}, guide_kind="normal")
     params = locate.learned_parameters_Clonal()
     return params
 
@@ -76,40 +85,57 @@ def run_hmm(data_input, gpu = False, nsteps = 400):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--indir", type=str, help="indir", default = "")
-    parser.add_argument("-s", "--sample", type=str, help="sample", default = "PDO61")
-    parser.add_argument("-c", "--chr", type=str, help="chromosome", default = "1")
-    parser.add_argument("-o", "--outdir", type=str, help="outdir", default = "")
+    parser.add_argument("-b", "--base", type=str, help="base", default = "")
+    parser.add_argument("-s", "--sim", type=str, help="sample", default = "sim_21")
+    
+    parser.add_argument("-p", "--ploidy", type=bool, help="ploidy", default = True)
+    parser.add_argument("-v", "--vaf", type=bool, help="vaf", default = False)  
+    parser.add_argument("-b", "--bps", type=bool, help="breakpoint", default = False)  
+
     args = parser.parse_args()
-
-    gpu = torch.cuda.is_available()
-
-    f = f'{args.indir}/{args.sample}_chr{args.chr}.csv'
-    data_ill = read_data(f, data_type = 'ill')
-    data_np = read_data(f, data_type = 'np')
- 
-    params_ill = run_hmm(data_ill, gpu = gpu)
-    params_np = run_hmm(data_np, gpu = gpu)
     
-    res_ill = pd.DataFrame({'CN_Major':params_ill["CN_Major"]+0.05,
-                           'CN_minor':params_ill["CN_minor"]-0.05,
-                            'baf':data_ill['baf'].view(-1).tolist(),
-                            'dr':data_ill['dr'].view(-1).tolist(),
-                            'original_baf':data_ill['orginal_baf'].view(-1).tolist(),
-                            'pos':[i for i in range(len(params_ill["CN_minor"]))],
-                            })
+    sim_dir = args.base + '/' + args.sim
+    combinations = [ i for i in os.listdir(sim_dir) if os.path.isdir(sim_dir + '/' + i)]
     
-    res_np = pd.DataFrame({'CN_Major':params_np["CN_Major"]+0.05,
-                           'CN_minor':params_np["CN_minor"]-0.05,
-                            'baf':data_np['baf'].view(-1).tolist(),
-                            'dr':data_np['dr'].view(-1).tolist(),
-                            'original_baf':data_np['orginal_baf'].view(-1).tolist(),
-                            'pos':[i for i in range(len(params_np["CN_minor"]))],
-                            })
-    
+    for comb in combinations:
+        data,data_input = read_data(f'{sim_dir}/{comb}/mirr_smooth_snv.csv')
+        ploidy, _ = estimate_ploidy(data, return_details=True)
+        
+        fix_ploidy = args.ploidy
+        vaf = args.vaf
+        bps = args.bps
+        
+        if not bps:
+            prior_bps = None
+        params = run_hmm(data_input, fix_ploidy=fix_ploidy, vaf=vaf, bps = prior_bps)
+        
+        name = f'vaf_{vaf}_ploidy_{ploidy}_bps_{bps}'
+        out_dir = f'{sim_dir}/{comb}/{name}'
+        os.makedirs(out_dir,exist_ok=True)
+        
+        inf_purity = params['purity']
+        if not fix_ploidy:
+            inf_ploidy = params['ploidy']
+        else:
+            inf_ploidy = None
+        
+        purity = comb.split('_')[-1]
+        parameters = pd.DataFrame({'purity':purity, 
+                                   'ploidy':ploidy, 
+                                   'inf_purity':inf_purity, 
+                                   'inf_ploidy':inf_ploidy})
+        parameters.to_csv(f'{out_dir}/params.csv', header=True)
 
-    os.makedirs(f'{args.outdir}/{args.sample}/', exist_ok=True)
-    res_ill.to_csv(f'{args.outdir}/{args.sample}/{args.sample}_chr{args.chr}_inference_ILL.csv', header=True)
-    res_np.to_csv(f'{args.outdir}/{args.sample}//{args.sample}_chr{args.chr}_inference_NP.csv', header=True)
+        res = pd.DataFrame({'CN_Major':params["CN_Major"],
+                    'CN_minor':params["CN_minor"],
+                    'pos':[i for i in range(len(params["CN_minor"]))]})
+        res.to_csv(f'{out_dir}/cna.csv', header=True)
+        
+        out_df = score_cn_predictions(data, res, sample_name=f'{args.sim}_{comb}')
+        out_df['summary'].to_csv(f'{out_dir}/summary.csv', header=True)
+        out_df['per_class_total'].to_csv(f'{out_dir}/summary_per_class_total.csv', header=True)
+        out_df['per_class_pair'].to_csv(f'{out_dir}/summary_per_class_pair.csv', header=True)
+        
+        
     
     
--- src/locate/utils.py
+++ src/locate/utils.py
@@ -14,6 +14,116 @@ from pyro.infer import SVI, TraceGraph_ELBO
 import torch
 from pandas.core.common import flatten
 import pyro
+from typing import Dict, Tuple, Optional
+
+
+def plot_purity_ploidy_hist(draws, bins=50):
+    fig, axs = plt.subplots(1, 2, figsize=(10,4))
+    if "purity" in draws:
+        axs[0].hist(draws["purity"].numpy(), bins=bins, density=True)
+        axs[0].set_title("Posterior of Purity")
+    if "ploidy" in draws:
+        axs[1].hist(draws["ploidy"].numpy(), bins=bins, density=True)
+        axs[1].set_title("Posterior of Ploidy")
+    plt.tight_layout()
+    plt.show()
+    
+def estimate_ploidy(
+    df: pd.DataFrame,
+    seg_id_col: str = "seg_id",
+    include_sex_chroms: bool = False,
+    valid_autosomes: Optional[set] = None,
+    return_details: bool = False) -> Tuple[float, Optional[pd.DataFrame]]:
+    """
+    Estimate sample ploidy from a per-variant dataframe annotated with segment IDs.
+
+    Assumes df[seg_id_col] has the form "chr:start:end:major:minor", e.g. "1:207651979:248956422:1:1".
+    Ploidy is computed as the length-weighted mean total copy number across unique segments:
+        ploidy = sum_i ( (major_i + minor_i) * length_i ) / sum_i length_i
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe (one row per variant), containing a column with segment IDs.
+    seg_id_col : str, default "seg_id"
+        Column name holding the segment identifier string.
+    include_sex_chroms : bool, default False
+        If False, excludes segments on sex chromosomes (X, Y, chrX, chrY, MT/M).
+    valid_autosomes : set or None
+        If provided, keep only chromosomes in this set (after stripping optional 'chr' prefix).
+        Example: set(map(str, range(1, 23))) for autosomes 1..22.
+    return_details : bool, default False
+        If True, also return a per-segment dataframe used in the calculation.
+
+    Returns
+    -------
+    ploidy : float
+        Estimated ploidy (length-weighted genome-wide total copy number).
+    details_df : Optional[pd.DataFrame]
+        If return_details=True, a dataframe with one row per unique segment:
+        columns: ['chr','start','end','length','major','minor','total_cn','weight'].
+    """
+    if seg_id_col not in df.columns:
+        raise ValueError(f"Column '{seg_id_col}' not found in dataframe.")
+
+    # Parse seg_id safely with a vectorized regex extraction
+    # Expected 5 fields: chr, start, end, major, minor
+    pat = r"^([^:]+):(\d+):(\d+):(\d+):(\d+)$"
+    parsed = df[seg_id_col].astype(str).str.extract(pat)
+    if parsed.isna().any().any():
+        bad = df.loc[parsed.isna().any(axis=1), seg_id_col].unique()[:5]
+        raise ValueError(
+            "Some seg_id values do not match 'chr:start:end:major:minor'. "
+            f"Examples of problematic entries: {list(bad)}"
+        )
+
+    parsed.columns = ["chr", "start", "end", "major", "minor"]
+    # Attach and drop duplicates so each segment is counted once
+    segs = parsed.copy()
+    segs[["start", "end", "major", "minor"]] = segs[["start", "end", "major", "minor"]].astype(np.int64)
+
+    # Normalize chromosome labels (e.g., 'chr1' -> '1')
+    def norm_chr(c):
+        c = str(c)
+        c = c[3:] if c.lower().startswith("chr") else c
+        return c
+
+    segs["chr_norm"] = segs["chr"].map(norm_chr)
+
+    # Optionally exclude sex/MT chromosomes
+    if not include_sex_chroms:
+        sex_like = {"x", "y", "mt", "m"}
+        segs = segs[~segs["chr_norm"].str.lower().isin(sex_like)]
+
+    # Optionally keep only specified autosomes
+    if valid_autosomes is not None:
+        segs = segs[segs["chr_norm"].isin(valid_autosomes)]
+
+    # Deduplicate identical segments
+    segs = segs.drop_duplicates(subset=["chr", "start", "end", "major", "minor"]).copy()
+
+    # Compute lengths and total copy number
+    segs["length"] = (segs["end"] - segs["start"] + 1).clip(lower=0)
+    segs = segs[segs["length"] > 0]
+
+    if segs.empty:
+        raise ValueError("No valid segments left after filtering; cannot estimate ploidy.")
+
+    segs["total_cn"] = segs["major"] + segs["minor"]
+    segs["weight"] = segs["length"].astype(float)
+
+    # Length-weighted mean total copy number
+    ploidy = float((segs["total_cn"] * segs["weight"]).sum() / segs["weight"].sum())
+
+    if return_details:
+        details = segs.rename(columns={"chr": "chr_raw"})
+        details = details[["chr_raw", "chr_norm", "start", "end", "length", "major", "minor", "total_cn", "weight"]]
+        details = details.reset_index(drop=True)
+        return ploidy, details
+    else:
+        return ploidy, None
+    
+    
 
 def plot_loss(loss, save = False, output = "run1"):
     """Plot loss function.
